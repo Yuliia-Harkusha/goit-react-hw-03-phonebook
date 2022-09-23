74c69e124b88a6a9d9d0a6bd5655bceadfcac45e
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-09-23 22:29:23 +0200

    add saving contacts in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,6 +24,24 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    console.log('didMount');
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts?.length) {
+      this.setState({
+        contacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('conponentDidUpdate');
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   addContact = contact => {
     if (this.isDuplicate(contact)) {
       return alert(`${contact.name} ${contact.number} is already in contacts`);
